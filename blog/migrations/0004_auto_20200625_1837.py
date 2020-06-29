b2c2dcdd9d6573cccfafafcf6725f099f53d0bd1
chandan kumar <ckamity03@gmail.com>
2020-06-29 18:56:50 +0530

    rename visiblity to visibility

--- blog/migrations/0004_auto_20200625_1837.py
+++ blog/migrations/0004_auto_20200625_1837.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='post',
-            name='visiblity',
+            name='visibility',
             field=models.IntegerField(choices=[(1, 'Public'), (2, 'Private')], default=1, max_length=1),
         ),
     ]
